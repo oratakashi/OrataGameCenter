4e8ae91c9b0f42f3572779fe81fc3ca0f7de0b8c
Oratakashi <davidcreazy@gmail.com>
2024-11-27 19:35:55 +0700

    feature: Revamp top navigation

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,5 +1,8 @@
  console.log("Hello from libraryroot.custom.js")
 
+ import { moveNavigationButton, moveMainTopNavigation } from './src/js/navigation.js'
+
  (async () => {
-    
+    moveNavigationButton();
+    moveMainTopNavigation();
  })();
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,1 +1,32 @@
-console.log("Hello from navigation.js")
+import { waitForElement } from './waitForElement.js';
+
+export async function moveNavigationButton() {
+    var topbar = await waitForElement('._39oUCO1OuizVPwcnnv88no');
+    var parent = await waitForElement('._2D64jIEK7wpUR_NlObDW76');
+
+    const navigationContainer = document.createElement('div');
+    navigationContainer.className = 'navigation-button';
+
+    const buttons = parent.querySelectorAll('svg');
+
+    buttons.forEach(button => {
+        navigationContainer.appendChild(button);
+    });
+
+    topbar.appendChild(navigationContainer);
+
+    navigationContainer.parentElement.prepend(navigationContainer);
+}
+
+export async function moveMainTopNavigation() {
+    var topbar = await waitForElement('._39oUCO1OuizVPwcnnv88no');
+    var navBar = await waitForElement('._2D64jIEK7wpUR_NlObDW76');
+    var anchor = await waitForElement('._30vB9DdsPK7VrZAbb5Q1Av');
+
+    // topbar.prepend(navBar);
+    topbar.insertBefore(navBar, anchor);
+
+    /**
+     * Add Hover State on menu for dynamic
+     */
+}
